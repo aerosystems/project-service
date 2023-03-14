a7fb672506d65a1c2b32ad8f6f8415a1a1ebc6cb
artk0st <art_k0st@icloud.com>
2023-03-14 02:19:06 +0200

    add /v1/projects/{id} GET METHOD

--- internal/handlers/projectRead.go
+++ internal/handlers/projectRead.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 )
@@ -11,11 +12,19 @@ func (h *BaseHandler) ProjectRead(w http.ResponseWriter, r *http.Request) {
 	projectID, err := strconv.Atoi(chi.URLParam(r, "projectID"))
 	if err != nil {
 		_ = WriteResponse(w, http.StatusBadRequest, NewErrorPayload(400001, "request is incorrect", err))
+		return
 	}
 
 	project, err := h.projectRepo.FindByID(projectID)
-	if err != nil {
+
+	if err != nil && err != gorm.ErrRecordNotFound {
 		_ = WriteResponse(w, http.StatusInternalServerError, NewErrorPayload(500001, "could not find Project by ProjectID", err))
+		return
+	}
+
+	if project == nil {
+		_ = WriteResponse(w, http.StatusNotFound, NewErrorPayload(404001, "Project not found", err))
+		return
 	}
 
 	payload := NewResponsePayload(fmt.Sprintf("project ID %d successfuly found", projectID), project)
