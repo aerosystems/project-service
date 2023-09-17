e4a872fd71ffdea40a29c39da9908d045bf79a27
artk0st <art_k0st@icloud.com>
2023-09-17 15:03:57 +0300

    add filters for project with role upper Startup

--- internal/rpc_server/handlers.go
+++ internal/rpc_server/handlers.go
@@ -15,14 +15,22 @@ func NewProjectServer(projectRepo models.ProjectRepository) *ProjectServer {
 	return &ProjectServer{projectRepo: projectRepo}
 }
 
-type RPCProjectPayload struct {
+type CreateProjectRPCPayload struct {
 	UserID     int
 	UserRole   string
 	Name       string
 	AccessTime time.Time
 }
 
-func (r *ProjectServer) CreateProject(payload RPCProjectPayload, resp *string) error {
+type ProjectRPCPayload struct {
+	ID         int
+	UserID     int
+	Name       string
+	Token      string
+	AccessTime time.Time
+}
+
+func (r *ProjectServer) CreateProject(payload CreateProjectRPCPayload, resp *string) error {
 	projectList, err := r.projectRepo.FindByUserID(payload.UserID)
 	if err != nil {
 		return err
@@ -50,5 +58,39 @@ func (r *ProjectServer) CreateProject(payload RPCProjectPayload, resp *string) e
 	}
 
 	*resp = fmt.Sprintf("project %s successfully created", payload.Name)
+	return nil
+}
+
+func (r *ProjectServer) GetProject(projectToken string, resp *ProjectRPCPayload) error {
+	project, err := r.projectRepo.FindByToken(projectToken)
+	if err != nil {
+		return err
+	}
+
+	*resp = ProjectRPCPayload{
+		ID:         project.ID,
+		UserID:     project.UserID,
+		Name:       project.Name,
+		Token:      project.Token,
+		AccessTime: project.AccessTime,
+	}
+	return nil
+}
+
+func (r *ProjectServer) GetProjectList(userID int, resp *[]ProjectRPCPayload) error {
+	projectList, err := r.projectRepo.FindByUserID(userID)
+	if err != nil {
+		return err
+	}
+
+	for _, project := range projectList {
+		*resp = append(*resp, ProjectRPCPayload{
+			ID:         project.ID,
+			UserID:     project.UserID,
+			Name:       project.Name,
+			Token:      project.Token,
+			AccessTime: project.AccessTime,
+		})
+	}
 	return nil
 }
